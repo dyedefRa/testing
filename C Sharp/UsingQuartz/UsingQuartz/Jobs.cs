68168e8bea8cf04f909e153c05d364317fbe27d1
Baki OZTURK <22.exe.94@gmail.com>
2019-04-30 14:13:02 +0300

    4-30-19

---
+++ C Sharp/UsingQuartz/UsingQuartz/Jobs.cs
@@ -0,0 +1,18 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingQuartz
+{
+    public class SendUserEmailsJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+            return Task.CompletedTask;
+        }
+    }
+}
