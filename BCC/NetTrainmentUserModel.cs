1e5381c876bdf4d3c3515b67994455077566211b
Baki OZTURK <22.exe.94@gmail.com>
2019-05-08 18:04:06 +0300

    5-8

---
+++ BCC/NetTrainmentUserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIPv2.Data.Domain.CustomRequestEntities
+{
+  public  class NetTrainmentUserModel
+    {
+        public string Identifier { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool IsNonUniqueEmail { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LanguageCode { get; set; }
+        public string AuthorizationToken { get; set; }
+        public long Expiration { get; set; }
+        public string CountryCode { get; set; }
+        public string ActivationCode { get; set; }
+        public StatusCode StatusCode { get; set; }
+    }
+    public enum StatusCode
+    {
+       Basarili=200,
+       YapilanIslemOlumsuz=333,
+       Bulunamadi=404,
+       Hata=666
+
+    }
+
+}
